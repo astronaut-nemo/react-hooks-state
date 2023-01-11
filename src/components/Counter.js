ffbd48bd57ac20a70d097b4b5faaa22ffbedba93
Joanna <astronautnemo@gmail.com>
2023-01-11 17:42:56 +0300

    completed lab

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,12 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount(count => (count + 1));
+    console.log('After first increment()', count);
+    setCount(count => (count + 1));
+    console.log('After second increment()', count);
   }
+  console.log('In component', count)
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
